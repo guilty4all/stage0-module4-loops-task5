376d6c030a878c90b930f12c05cab094490bc3aa
guilt <gulilty4all@gmail.com>
2022-09-05 17:55:04 +0600

    solution some fixes1

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -3,8 +3,11 @@ package school.mjc.stage0.loops.task5;
 public class Triangle {
     public void printTriangle(int cathetusLength) {
         String symbol = "8";
-        for(int i = 0; i <= cathetusLength+1;i++){
-            System.out.println(symbol.repeat(i));
+        int space = cathetusLength;
+        for(int i = 1; i <= cathetusLength;i++){
+            String word = symbol.repeat(i)+" ".repeat(--space);
+            System.out.println(word);
         }
+
     }
 }
